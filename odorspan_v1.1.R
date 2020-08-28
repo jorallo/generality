f1be7bba920958ea95b5f309e08e5473fe2c6dbb
jorallo <jorallo@dsic.upv.es>
2020-08-28 23:36:15 +0200

    Update odorspan_v1.1.R

--- odorspan_v1.1.R
+++ odorspan_v1.1.R
@@ -4,9 +4,7 @@
 #
 ##############################################################################
 # 
-# This is R code for doing Generality Analysis, based on the metric of generality first introduced in 
-#   J. Hernandez-Orallo "I.G.", March 15th, 2018
-#   https://riunet.upv.es/bitstream/handle/10251/100267/secondbest.pdf
+# This is R code for doing Generality Analysis, based on the metric of generality
 #
 # Data from experiment 2:
 # L. Brooke April, Katherine Bruce, Mark Galizio "The magic number 70 (plus or minus 20): Variables determining performance in the Rodent Odor Span Task", Learning and Motivation 44 (2013) 143- 158
@@ -37,8 +35,7 @@
 ##############################################################################
 
 
-
-setwd("/OneDrive - UPV/__SUBMISSIONS/2018/General intelligence/__NewCode__")
+setwd("/.../ **** PUT YOUR FOLDER HERE ****")
 source("generality.R")
 
 DATADIR <- "odor.span.task.data"
